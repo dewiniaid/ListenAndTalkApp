ac2769dc906c8b397ff1040d73c807f95d7deaf7
Jack Chang <wei0831@gmail.com>
2015-10-03 14:30:50 -0700

    Fix missing comma

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -2,16 +2,16 @@ module.exports = (function(){
     return {
       getAllStudents : function(req, res){
         res.status(200).send("All the students are returned");
-      }
+      },
       getStudentById : function(req, res){
         res.status(200).send("Student returned by ID");
-      }
+      },
       putStudentById : function(req, res){
         res.status(200).send("Student are put on this endpoint");
-      }
+      },
       addNewStudent : function(req, res){
         res.status(200).send("Student are added on this endpoint");
-      }
+      },
       getStudentsActivities : function(req, res){
         res.status(200).send("Find students by activities");
       }
