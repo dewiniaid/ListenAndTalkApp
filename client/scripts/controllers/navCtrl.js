55732ee274479ed36533cf04c4a44dbef6767135
Jack Chang <wei0831@gmail.com>
2015-10-03 14:38:15 -0700

    Add important comment

--- client/scripts/controllers/navCtrl.js
+++ client/scripts/controllers/navCtrl.js
@@ -3,6 +3,7 @@ var app = angular.module('app');
 app.controller('navCtrl', function($scope, $location){
     $scope.init = function(){
       $scope.navList = [
+        {name: "Test", href: "#"},
         {name: "About", href: "#/about"}
       ];
     };
