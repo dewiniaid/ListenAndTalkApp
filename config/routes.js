089c1e1a9316e2c621f0f04c0a7b5c1a2a82db6a
Mihir Shah <Mihir@Mihirs-MacBook-Pro.local>
2015-10-03 14:05:57 -0700

    adding preliminary routes and controllers

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,23 @@
 var test = require("../server/controllers/cTest.js");
+var students = require("../server/controllers/students.js");
 
 module.exports = function(app) {
   app.get('/api/test', function(req, res) {
     test.test(req, res);
+  });
+  app.get('/api/v1/students', function(req, res){
+    students.getAllStudents(req, res);
+  });
+  app.get('/api/v1/students/:id', function(req, res){
+    students.getStudentById(req, res);
+  });
+  app.put('/api/v1/students/:id', function(req, res){
+    students.putStudentById(req, res);
+  });
+  app.post('/api/v1/students', function(req, res){
+    students.addNewStudent(req, res);
+  });
+  app.get('/api/v1/students/:id/activities', function(req, res){
+    students.getStudentsActivities(req, res);
   });
 };
