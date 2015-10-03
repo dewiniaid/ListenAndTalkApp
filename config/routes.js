9bc409925b2c826d3f31b8bb6c5f29400836c3df
Mihir Shah <Mihir@Mihirs-MacBook-Pro.local>
2015-10-03 14:14:21 -0700

    adding teachers

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 var test = require("../server/controllers/cTest.js");
 var students = require("../server/controllers/students.js");
+var teachers = require("../server/controllers/teachers.js");
 
 module.exports = function(app) {
   app.get('/api/test', function(req, res) {
@@ -19,5 +20,14 @@ module.exports = function(app) {
   });
   app.get('/api/v1/students/:id/activities', function(req, res){
     students.getStudentsActivities(req, res);
+  });
+  app.get('/api/v1/teachers/:id', function(req, res){
+    teachers.getTeacherInfo(req, res);
+  });
+  app.put('/api/v1/teachers/:id', function(req, res){
+    teachers.updateTeacherInfo(req, res);
+  });
+  app.post('/api/v1/teachers', function(req, res){
+    teachers.addNewTeacher(req, res);
   });
 };
