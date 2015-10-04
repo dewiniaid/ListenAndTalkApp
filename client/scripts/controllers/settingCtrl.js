ee7dbfbbd16380140b336bb3821ee1f2dcab6995
Brian Finck <brian@brianfinck.com>
2015-10-04 03:28:48 -0700

    working on setting function

--- client/scripts/controllers/settingCtrl.js
+++ client/scripts/controllers/settingCtrl.js
@@ -2,20 +2,20 @@ var app = angular.module('app');
 
 app.controller('settingCtrl', function($scope, mainFactory, store, $window, $state) {
 
-//	mainFactory.test(function(result) {
-//		$scope.test = result;
-//	});
-//
-//	$scope.post = function() {
-//		mainFactory.test_post($scope.postData, function(result) {
-//			console.log(result);
-//		});
+	mainFactory.getTeachers(function(result) {
+		$scope.students = result;
+	});
+
+
+//	var studentsToCheckIn = {};
+//	$scope.checkin = function(status, studentID) {
+//		// studentsToCheckIn.push({"status": status, "studentID": studentID})
+//		studentsToCheckIn[studentID] = status;
+//		console.log(studentsToCheckIn);
+//		if (Object.keys(studentsToCheckIn).length == $scope.students.length) {
+//			mainFactory.checkIn(function() {
+//				console.log('test');
+//			})
+//		};
 //	}
-//
-//	$scope.friends = [{name:'John', phone:'555-1276'},
-//										{name:'Mary', phone:'800-BIG-MARY'},
-//										{name:'Mike', phone:'555-4321'},
-//										{name:'Adam', phone:'555-5678'},
-//										{name:'Julie', phone:'555-8765'},
-//										{name:'Juliette', phone:'555-5678'}];
 });
