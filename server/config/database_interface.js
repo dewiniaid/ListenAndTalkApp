582f56fab1aa6aee45e42d35fc11b9ab58a298a4
Chris Tran <christopher.huytran@gmail.com>
2015-10-04 11:25:26 -0700

    ng if

--- server/config/database_interface.js
+++ server/config/database_interface.js
@@ -215,7 +215,8 @@ module.exports = function() {
 
     getActivityByTeacherEmailQuery : function(email){
       return {
-          text: "SELECT * FROM "+ACTIVITY_TABLE +" WHERE staff_id = (SELECT id FROM " + TEACHER_TABLE + " WHERE email = $1)",
+          // text: "SELECT * FROM "+ACTIVITY_TABLE +" WHERE staff_id = (SELECT id FROM " + TEACHER_TABLE + " WHERE email = $1)",
+          text: "SELECT * FROM " + ACTIVITY_TABLE +" WHERE staff_id = (SELECT id FROM " + TEACHER_TABLE + " WHERE email = $1)",
           values: [email],
           name: 'teacherEmail'
       };
