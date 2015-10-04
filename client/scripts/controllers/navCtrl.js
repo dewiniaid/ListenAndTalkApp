75d47e3a76dcff8e6eea90b0b2b5d0e4c9bf7b7b
Jack Chang <wei0831@gmail.com>
2015-10-04 07:49:22 -0700

    Remove test stuff

--- client/scripts/controllers/navCtrl.js
+++ client/scripts/controllers/navCtrl.js
@@ -3,7 +3,6 @@ var app = angular.module('app');
 app.controller('navCtrl', function($scope, $location){
     $scope.init = function(){
       $scope.navList = [
-        {name: "Test", href: "#/test"},
         {name: "Mark Attendance", href: "#/markAttendance"},
         {name: "View Attendance", href: "#/viewAttendance"},
         {name: "Settings", href: "#/settings"}
