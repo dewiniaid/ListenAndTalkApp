a56c7393b88e1c253f8d2149292aa93836330588
Brian Finck <brian@brianfinck.com>
2015-10-04 04:44:28 -0700

    working on deactivate function

--- client/scripts/controllers/settingCtrl.js
+++ client/scripts/controllers/settingCtrl.js
@@ -3,9 +3,12 @@ var app = angular.module('app');
 app.controller('settingCtrl', function($scope, mainFactory, store, $window, $state) {
 
 	mainFactory.getTeachers(function(result) {
-		$scope.students = result;
+		$scope.teachers = result;
 	});
 
+	mainFactory.getAllStudents(function(result) {
+		$scope.students = result;
+	});
 
 	
 //	var teacherDeactivateList = {};
