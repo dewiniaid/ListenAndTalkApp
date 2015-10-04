2654268df54306ade8c4ecc3f1b45e339add61e7
Daniel Grace <daniel@wa.childcareaware.org>
2015-10-04 14:13:31 -0700

    Remove dead code.

--- server/config/database_interface.js
+++ server/config/database_interface.js
@@ -197,15 +197,6 @@ module.exports = function() {
       };
     },
 
-/*
-    addNewStudentQuery : function(firstName, lastName){
-      return {
-          text: "INSERT INTO "+ STUDENT_TABLE +"(name_first, name_last) VALUES ($1, $2)",
-          values: [firstName, lastName],
-          name: 'adding new student by name'
-      };
-    },
-*/	
 	addNewStudentQuery : StudentUpdater.generateInsertQuery(),
 	updateStudentQuery : StudentUpdater.generateUpdateQuery(),
 
@@ -216,15 +207,6 @@ module.exports = function() {
           name: 'remove student by id'
       };
     },
-/*
-    addNewTeacherQuery : function(firstName, lastName, email){
-      return {
-          text: "INSERT INTO "+ TEACHER_TABLE +"(name_first, name_last, email) VALUES ($1, $2, $3)",
-          values: [firstName, lastName, email],
-          name: 'adding new student by name'
-      };
-    },
-*/
 	addNewTeacherQuery : StaffUpdater.generateInsertQuery(),
 	updateTeacherQuery : StaffUpdater.generateUpdateQuery(),
 
