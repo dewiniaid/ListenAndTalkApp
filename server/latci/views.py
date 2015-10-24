764cb023f81414b4d99952cf3aef08ad8bf12f96
Daniel Grace <thisgenericname@gmail.com>
2015-10-23 23:58:17 -0700

    Add Staff and Activity views.

--- server/latci/views.py
+++ server/latci/views.py
@@ -40,4 +40,47 @@ class StudentRestController(SimpleIDRestController, rest.SortableRESTController,
             dump_only=('date_inactive', 'date_created')
         )
 
+
+# noinspection PyAbstractClass
+class StaffRestController(SimpleIDRestController, rest.SortableRESTController, rest.InactiveFilterRESTController):
+    model = models.Staff
+    name = 'staff'
+
+    allow_fetch = True
+    allow_delete = False
+    allow_delete_all = False
+    allow_create = True
+    allow_update = True
+    allow_replace = False
+    treat_put_as_patch = True
+    sortable_columns = {v: [v] for v in ('name_first', 'name_last', 'id')}
+
+    def get_schema(self):
+        return self.SchemaClass(
+            exclude=('id', 'activities'),
+            dump_only=('date_inactive', 'date_created')
+        )
+
+
+# noinspection PyAbstractClass
+class ActivityRestController(SimpleIDRestController, rest.SortableRESTController, rest.InactiveFilterRESTController):
+    model = models.Activity
+    name = 'activity'
+
+    allow_fetch = True
+    allow_delete = False
+    allow_delete_all = False
+    allow_create = True
+    allow_update = True
+    allow_replace = False
+    treat_put_as_patch = True
+    sortable_columns = {v: [v] for v in ('start_date', 'end_date', 'name')}  # TODO: Make more useful.
+
+    def get_schema(self):
+        return self.SchemaClass(
+            exclude=('id', 'enrollment', 'staff', 'location', 'category'),
+            dump_only=('date_inactive', 'date_created')
+        )
+
+
 rest.setup_all()
